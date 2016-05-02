dbe57f32a43aab909f1d46184b228183683d10c9
joe <jcaprino@uncc.edu>
2016-05-01 22:49:07 -0400

    Added copyright details and program description for PMF code

--- src/cpp/PMF_Recommender_System.cpp
+++ src/cpp/PMF_Recommender_System.cpp
@@ -1,11 +1,24 @@
-//============================================================================
-// Name        : Bayesian.cpp
-// Author      : Joseph Caprino
-// Version     :
-// Copyright   :
-// Description : Bayesian Probabilistic Matrix Factorization based on
-// code at http://www.cs.toronto.edu/~rsalakhu/code_BPMF/pmf.m
-//============================================================================
+// Description : Probabilistic Matrix Factorization based on
+// original code at http://www.cs.toronto.edu/~rsalakhu/code_BPMF/pmf.m
+// main page of original programs: http://www.cs.toronto.edu/~rsalakhu/BPMF.html
+
+//% Original MATLAB Code provided by Ruslan Salakhutdinov
+// Translation to C++ by Joseph Caprino
+//%
+//% Permission is granted for anyone to copy, use, modify, or distribute this
+//% program and accompanying programs and documents for any purpose, provided
+//% this copyright notice is retained and prominently displayed, along with
+//% a note saying that the original programs are available from our
+//% web page.
+//% The programs and documents are distributed without any warranty, express or
+//% implied.  As the programs were written for research purposes only, they have
+//% not been tested to the degree that would be advisable in any important
+//% application.  All use of these programs is entirely at the user's own risk.
+
+
+
+
+
 
 #include <iostream>
 #include <algorithm>
