a4f8d12d4164093d2d123d7395280c94165f52c4
joe <jcaprino@uncc.edu>
2016-05-07 17:03:14 -0400

    Edited the rating schema to reflect the limitations of the MySQL C++ Connector. The C++ Connector does not support FLOAT values, so the float values have been changed to doubles for compatibility and ease of use. Additionally, the user_id has been changed to customer_id becaus it makes more sense.

--- setup/ratingSchema.sql
+++ setup/ratingSchema.sql
@@ -2,10 +2,10 @@ CREATE DATABASE IF NOT EXISTS `rating_recommending`;
 USE `rating_recommending`;
 DROP TABLE IF EXISTS `rating`;
 CREATE TABLE `rating` (
-`rating_value` float NOT NULL,
-`user_id` INT UNSIGNED NOT NULL, 
+`rating_value` double NOT NULL,
+`customer_id` INT UNSIGNED NOT NULL, 
 `product_id` INT UNSIGNED NOT NULL,
-PRIMARY KEY (`user_id`, `product_id` ),
-FOREIGN KEY (`user_id` ) REFERENCES affablebean.customer(`id`),
+PRIMARY KEY (`customer_id`, `product_id` ),
+FOREIGN KEY (`customer_id` ) REFERENCES affablebean.customer(`id`),
 FOREIGN KEY (`product_id` ) REFERENCES affablebean.product(`id`)
  );
