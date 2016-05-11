c3dd80e5aeef66ebec1e039c47c963bd87c701c1
joecaprino <jcaprino@uncc.edu>
2016-05-11 00:38:34 -0400

    Update ratingSchema_PMF_Loading_Sample_Training_Data.sql

--- setup/ratingSchema_PMF_Loading_Sample_Training_Data.sql
+++ setup/ratingSchema_PMF_Loading_Sample_Training_Data.sql
@@ -1,3 +1,5 @@
+-- This file is an exmaple file for loading recommender system training data into
+-- a training database called rating_recommending_pmf_test
 USE rating_recommending_pmf_test;
 -- help from here: http://dev.mysql.com/doc/refman/5.1/en/load-data.html
 LOAD DATA INFILE '/path/to/pmftraining.csv' 
